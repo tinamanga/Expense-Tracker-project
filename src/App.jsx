de1dd9f7f7e199b8613308a442c68ebb4d305b4d
tinamanga <tinamanga@gmail.com>
2025-04-12 15:59:26 +0300

    updated my App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+
+import { useState } from "react";
+import { initialExpenses } from "./data/dummyData";
+import ExpenseTable from "./components/ExpenseTable";
+
 function App() {
+
+    const [expenses, setExpenses] = useState(initialExpenses);
     return (
       <div className="App">
         <h1>Expense Tracker 💰</h1>
+        <ExpenseTable expenses={expenses} />
       </div>
     );
   }
